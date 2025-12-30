e4a47bf54fd6269315e6f7926979487df2fb5777
Vinod Morusu <morusu.vinod@gmail.com>
2025-12-30 22:03:43 +0530

    commit pjct

---
+++ Day14 - Mini Pjct-1/output.tf
@@ -0,0 +1,11 @@
+# output "instance_id" {
+#   value = aws_instance.main.id
+# }
+
+# output "instance_private_ip" {
+#   value = aws_instance.main.private_ip
+# }
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
